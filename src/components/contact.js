37f01604f860ac600ec3d7a2dc5673f5557e3140
Seraphin <aribi.rachid@hotmail.fr>
2020-11-18 17:39:17 +0100

    adapted mail to button for smartphone

--- src/components/contact.js
+++ src/components/contact.js
@@ -8,7 +8,7 @@ const Contact = () => {
                 <p id="title-contact">GET IN TOUCH !</p>
                 <a className="mail-btn" href="gotoline" target="_blank"> <img src={Logomail} className="mail-logo" alt="mail-logo"></img> </a>
                 <p id="contact-sentence">Whether you have an idea for a project or just want to chat, feel free to shoot me an email!</p> 
-                <a id="email-button" href="mailto:solidaritydev93@gmail.com" target="_blank" rel="noopener noreferrer">Say Hello :)</a> 
+                <a id="email-button" href="mailto:solidaritydev93@gmail.com?" target="_blank" rel="noopener noreferrer">Say Hello :)</a> 
             </div>
     
     )
