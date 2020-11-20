54fdabd7c3c9c877f6a49a952aa73f40f59d48c8
Seraphin <aribi.rachid@hotmail.fr>
2020-11-20 16:08:00 +0100

    changed syntax on email

--- src/components/contact.js
+++ src/components/contact.js
@@ -5,7 +5,7 @@ const Contact = () => {
     return (
       
             <div className="contact-section">
-                <p id="title-contact">GET IN TOUCH !</p>
+                <p id="title-contact">GET IN TOUCH!</p>
                 <a className="mail-btn" href="gotoline" target="_blank"> <img src={Logomail} className="mail-logo" alt="mail-logo"></img> </a>
                 <p id="contact-sentence">Whether you have an idea for a project or just want to chat, feel free to shoot me an email!</p> 
                 <a id="email-button" href="mailto:solidaritydev93@gmail.com?" target="_blank" rel="noopener noreferrer">Say Hello :)</a> 
